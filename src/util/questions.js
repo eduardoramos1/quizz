b65179c38c2c672b581fe60439cedcdf9b33c2f4
Eduardo Ramos <eduardorxs@gmail.com>
2019-09-21 21:13:31 -0300

    Versão inicial

---
+++ src/util/questions.js
@@ -0,0 +1,83 @@
+export default [
+    {
+        text: 'Qual é o coletivo de cães?',
+        answers: [
+            { text: 'MATILHA', correct: true },
+            { text: 'REBANHO', correct: false },
+            { text: 'CARDUME', correct: false },
+            { text: 'MANADA', correct: false }
+        ]
+    },
+    {
+        text: 'Qual é a maior floresta do planeta?',
+        answers: [
+            { text: 'NEGRA', correct: false },
+            { text: 'DE SHERWOOD', correct: false },
+            { text: 'DA TIJUCA', correct: false },
+            { text: 'AMAZÔNICA', correct: true }
+        ]
+    },
+    {
+        text: 'Quem é o primeiro substituto do Presidente?',
+        answers: [
+            { text: 'VEREADOR', correct: false },
+            { text: 'VICE-PRESIDENTE', correct: true },
+            { text: 'SENADOR', correct: false },
+            { text: 'DEPUTADO', correct: false }
+        ]
+    },
+    {
+        text: 'Segundo o romance, que animal era “Moby Dick”?',
+        answers: [
+            { text: 'TUBARÃO', correct: false },
+            { text: 'GOLFINHO', correct: false },
+            { text: 'POLVO', correct: false },
+            { text: 'BALEIA', correct: true }
+        ]
+    },
+    {
+        text: 'Que animal é o Pateta?',
+        answers: [
+            { text: 'CAVALO', correct: false },
+            { text: 'BURRO', correct: false },
+            { text: 'CACHORRO', correct: true },
+            { text: 'RAPOSA', correct: false }
+        ]
+    },
+    {
+        text: 'Qual destes personagens não veste uma capa?',
+        answers: [
+            { text: 'SUPER-HOMEM', correct: false },
+            { text: 'BATMAN', correct: false },
+            { text: 'WOLVERINE', correct: true },
+            { text: 'ZORRO', correct: false }
+        ]
+    },
+    {
+        text: 'Qual empresa criou o jogo "Mario Bros" ? ',
+        answers: [
+            { text: 'NINTENDO', correct: true },
+            { text: 'SONY', correct: false },
+            { text: 'MICROSOFT', correct: false },
+            { text: 'SEGA', correct: false }
+        ]
+    },
+    {
+        text: 'Em que continente fica a Espanha ?',
+        answers: [
+            { text: 'AMÉRICA DO SUL', correct: false },
+            { text: 'AMÉRICA DO NORTE', correct: false },
+            { text: 'EUROPA', correct: true },
+            { text: 'OCEANIA', correct: false }
+        ]
+    },
+    {
+        text: 'Qual a moeda corrente do Japão',
+        answers: [
+            { text: 'DÓLAR', correct: false },
+            { text: 'YEN', correct: true },
+            { text: 'EURO', correct: true },
+            { text: 'YUAN', correct: false }
+        ]
+    },
+]
